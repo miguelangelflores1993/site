ad5386bc634c12c01d29f1753a163e6f40ba2e1b
Miguel Angel Flores <miguel@mobi.lat>
2025-03-09 08:56:58 -0300

    Update steller.js

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -1,15 +1,32 @@
-<script>
-  // Función para desplazamiento suave
-  function smoothScroll(event) {
-    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    const targetId = event.target.getAttribute('href'); // Obtiene el hash del enlace
-    const targetElement = document.querySelector(targetId); // Selecciona el elemento objetivo
+/*!
+=========================================================
+* Steller Landing page
+=========================================================
 
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth', // Desplazamiento suave
-        block: 'start'       // Alinea el elemento en la parte superior de la ventana
-      });
-    }
-  }
-</script>
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+});
